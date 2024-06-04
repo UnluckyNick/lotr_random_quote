0ab993eccc2c873cf9102381d26737c8cb79d000
UnluckyNick <nickwoelk3d@gmail.com>
2024-06-03 23:27:26 -0400

    get only dialog from response

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import requests
 import config
+import json
 
 headers = {"Authorization": f"Bearer {config.api_key}"}
 
 response = requests.get("https://the-one-api.dev/v2/quote", headers=headers)
 
-print(response.json())
+the_one_list = response.json()
+
+#for key in the_one_list:{
+#    print(key,":", the_one_list['docs'])
+#}
+
+dialog = the_one_list['docs'][0]['dialog']
+print(f"dialog: {dialog}")
